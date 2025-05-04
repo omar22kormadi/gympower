5c1b9f161de1889e1fd64786f854987cb15ab110
Omar Kormadi <amorkormadi@gmail.com>
2025-05-04 11:20:32 +0100

    article-list done , (for now my data is located in article.service.ts)

--- src/app/article-item/article-item.component.ts
+++ src/app/article-item/article-item.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Article } from '../model/article.model';
 
 @Component({
   selector: 'app-article-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './article-item.component.html',
   styleUrl: './article-item.component.css'
 })
 export class ArticleItemComponent {
-
+  @Input() article!: Article;
+  
+  constructor() { }
 }
--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Article } from '../model/article.model';
+import { RouterModule } from '@angular/router';
+import { ArticleItemComponent } from '../article-item/article-item.component';
+import { CommonModule } from '@angular/common';
+import { ArticleService } from '../services/articles.service';
 
 @Component({
   selector: 'app-article-list',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, ArticleItemComponent, CommonModule],
   templateUrl: './article-list.component.html',
   styleUrl: './article-list.component.css'
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
+  articles: Article[] = [];
+  currentPage: number = 1;
+  totalPages: number = 1;
+  
+  constructor(private articleService: ArticleService) { }
 
+  ngOnInit(): void {
+    this.loadArticles();
+    this.totalPages = this.articleService.getTotalPages();
+  }
+
+  loadArticles(): void {
+    this.articles = this.articleService.getArticles(this.currentPage);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.currentPage++;
+      this.loadArticles();
+    }
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadArticles();
+    }
+  }
 }
