a45570f24ba9f000f564db581d1d79e460529048
Omar Kormadi <amorkormadi@gmail.com>
2025-05-04 10:41:27 +0100

    header component added

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  title: string = 'GYM POWER';
+  
+  navItems: {name: string, link: string}[] = [
+    { name: 'Home', link: '#' },
+    { name: 'Products', link: '#' },
+    { name: 'About', link: '#' },
+    { name: 'Contact', link: '#' }
+  ];
 }
