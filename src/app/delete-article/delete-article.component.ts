e43644af19b3fd3364f04714288c4749535d4e21
Omar Kormadi <amorkormadi@gmail.com>
2025-05-04 16:03:41 +0100

    add, edit and delete empty components added

---
+++ src/app/delete-article/delete-article.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-article',
+  standalone: true,
+  imports: [],
+  templateUrl: './delete-article.component.html',
+  styleUrl: './delete-article.component.css'
+})
+export class DeleteArticleComponent {
+
+}
